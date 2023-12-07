6c35f020001d72eeb701777c366f0a205828af05
OQASergey <sergey.qa.63@protonmail.com>
2023-12-07 18:44:17 +0400

    Обновлен conftest.py - добавлена конфигурация для браузера ('--headless')

--- conftest.py
+++ conftest.py
@@ -1,8 +1,13 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 @pytest.fixture(scope='module', autouse=True)
 def browser_management():
+    '''
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options'''
     browser.open()
     print('Браузер открыт')
     browser.config.base_url = 'https://todomvc.com/examples/'
