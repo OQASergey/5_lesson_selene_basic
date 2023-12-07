f3b8074914245989fb1efa6d20777ed9b32eece3
OQASergey <sergey.qa.63@protonmail.com>
2023-12-07 22:39:26 +0400

    Незначитально обновлен test_todos.py - добавлен тест через exact_texts

--- test_todos.py
+++ test_todos.py
@@ -15,3 +15,8 @@ def test_type_todo():
 
 def test_complite():
     browser.all('[id="todo-list"]>li').should(have.size(3))
+    browser.all('[id="todo-list"]>li').element_by(have.exact_texts('a', 'b'))
+
+
+#def test_delete():
+#    browser.all('[id="new-todo"]>li').element_by(have.exact_text('c')).element('.toggle').click()
