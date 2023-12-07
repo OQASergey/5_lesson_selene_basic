41471b5643d0f46f0729590ac015f6e8c3580e12
OQASergey <sergey.qa.63@protonmail.com>
2023-12-07 22:37:18 +0400

    Незначитально обновлен conftest.py

--- conftest.py
+++ conftest.py
@@ -4,10 +4,10 @@ from selenium import webdriver
 
 @pytest.fixture(scope='module', autouse=True)
 def browser_management():
-    '''
+
     driver_options = webdriver.ChromeOptions()
     driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options'''
+    browser.config.driver_options = driver_options
 
 
     browser.open()
